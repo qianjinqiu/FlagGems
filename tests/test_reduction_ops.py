a296ffe63e9d7ff9a51d01578a26b95d3e1c9baa
qianjinqiu <37774042+qianjinqiu@users.noreply.github.com>
2025-06-05 20:04:00 +0800

    Update test_reduction_ops.py 12

--- tests/test_reduction_ops.py
+++ tests/test_reduction_ops.py
@@ -125,6 +125,7 @@ def test_accuracy_argmin(shape, dim, keepdim, dtype):
 @pytest.mark.skipif(flag_gems.vendor_name == "kunlunxin", reason="RESULT TODOFIX")
 @pytest.mark.CrossEntropyLoss
 def test_accuracy_cross_entropy_loss_indices():
+    shape = [3,3]
     dim = 1
     up_limit = shape[dim] - 1
     target_shape = list(shape)
