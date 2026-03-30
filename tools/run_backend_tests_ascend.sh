27626355cf9157a03d358db56fe2d6e1fcbf817c
Qiming Teng <tengqm@outlook.com>
2026-03-30 14:07:15 +0800

    Try enable all accuracy tests on Ascend (#2181)

--- tools/run_backend_tests_ascend.sh
+++ tools/run_backend_tests_ascend.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
+# For testing
+CHANGED_FILES="__ALL__"
+
 VENDOR=${1:?"Usage: bash tools/run_backend_tests_ascend.sh <vendor>"}
 export GEMS_VENDOR=$VENDOR
 export TRITON_ALL_BLOCKS_PARALLEL=1
+
+if [[ "$CHANGED_FILES" == "__ALL__" ]]; then
+  # Replace "__ALL__" with all tests
+  CHANGED_FILES=$(find tests -name "test*.py")
+  EXTRA_OPTS=""
+else
+  # for per-PR test, fail early
+  EXTRA_OPTS="-x"
+fi
+
+TEST_CASES=()
+for item in $CHANGED_FILES; do
+  case $item in
+    # tests/test_DSA/*)
+    # skip DSA test for now
+    #  ;;
+    # tests/test_quant.py)
+    # skip because it always fail
+    #  ;;
+    tests/*) TEST_CASES+=($item)
+  esac
+done
+
+# Skip tests if no tests file is found
+if [ ${#TEST_CASES[@]} -eq 0 ]; then
+  exit 0
+fi
 
 # Initialize Ascend environment variables.
 # This script is provided by the Huawei Ascend CANN toolkit installation.
@@ -14,8 +44,4 @@ source tools/run_command.sh
 
 echo "Running FlagGems tests with GEMS_VENDOR=$VENDOR"
 
-# run_command python3 -m pytest -s tests/test_tensor_constructor_ops.py -k "not test_accuracy_randperm"
-run_command python3 -m pytest -s tests/test_libentry.py
-run_command python3 -m pytest -s tests/test_shape_utils.py
-# run_command python3 -m pytest -s tests/test_tensor_wrapper.py
-# run_command python3 -m pytest -s tests/test_distribution_ops.py
+python3 -m pytest -s ${EXTRA_OPTS} ${TEST_CASES[@]}
