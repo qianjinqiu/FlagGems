556efe696501a83c66b7072d11197e6981b038a5
Qiming Teng <tengqm@outlook.com>
2026-04-05 14:07:38 +0800

    Trivial update for CI testing (#2233)

--- tools/run_backend_tests_iluvatar.sh
+++ tools/run_backend_tests_iluvatar.sh
@@ -18,4 +18,33 @@ source .venv/bin/activate
 uv pip install setuptools==82.0.1 scikit-build-core==0.12.2 pybind11==3.0.3 cmake==3.31.10 ninja==1.13.0
 uv pip install -e .[iluvatar,test]
 
-pytest -s tests
+source tools/run_command.sh
+
+# Reduction ops
+run_command pytest -s tests/test_reduction_ops.py
+run_command pytest -s tests/test_general_reduction_ops.py
+run_command pytest -s tests/test_norm_ops.py
+
+# Pointwise ops
+run_command pytest -s tests/test_pointwise_dynamic.py
+run_command pytest -s tests/test_unary_pointwise_ops.py
+run_command pytest -s tests/test_binary_pointwise_ops.py
+run_command pytest -s tests/test_pointwise_type_promotion.py
+run_command pytest -s tests/test_tensor_constructor_ops.py
+
+# BLAS ops
+# TODO(Qiming): Fix sharedencoding on Hopper
+run_command pytest -s tests/test_attention_ops.py
+run_command pytest -s tests/test_blas_ops.py
+
+# Special ops
+run_command pytest -s tests/test_special_ops.py
+run_command pytest -s tests/test_distribution_ops.py
+
+# Convolution ops
+run_command pytest -s tests/test_convolution_ops.py
+
+# Utils
+run_command pytest -s tests/test_libentry.py
+run_command pytest -s tests/test_shape_utils.py
+run_command pytest -s tests/test_tensor_wrapper.py
