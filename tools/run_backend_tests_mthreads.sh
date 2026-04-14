b87fac837327d5278d4015ec500ec42e0cb84ffc
Qiming Teng <tengqm@outlook.com>
2026-04-14 15:34:44 +0800

    Add FlagTree lib path for mthreads (#2432)

--- tools/run_backend_tests_mthreads.sh
+++ tools/run_backend_tests_mthreads.sh
@@ -33,6 +33,8 @@ export PATH=$MUSA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$MUSA_HOME/lib:$LD_LIBRARY_PATH
 # For the intel math library
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
+# For the Flagtree dynamic library
+export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib/python3.10/site-packages/triton/_C:$LD_LIBRARY_PATH
 
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
