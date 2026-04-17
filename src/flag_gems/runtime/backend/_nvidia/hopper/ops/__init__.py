3d70affb9ef655f1d373cc5e3e81f2a3e5dac1c5
Vincent Xiao <vincent.xiao.me@gmail.com>
2026-04-17 15:45:51 +0800

    Optimized hopper w8a8_block_fp8_matmul with up to 4.06× speedup (#2490)

--- src/flag_gems/runtime/backend/_nvidia/hopper/ops/__init__.py
+++ src/flag_gems/runtime/backend/_nvidia/hopper/ops/__init__.py
@@ -11,5 +11,6 @@ if triton.__version__ >= "3.4":
     )
     from .mm import mm, mm_out  # noqa: F401
     from .sqrt import sqrt, sqrt_  # noqa: F401
+    from .w8a8_block_fp8_matmul import w8a8_block_fp8_matmul  # noqa: F401
 
 __all__ = ["*"]
