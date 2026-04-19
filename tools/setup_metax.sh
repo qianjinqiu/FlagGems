1ba51ca035824e268974494ecfa2089c5c002959
Qiming Teng <tengqm@outlook.com>
2026-04-19 13:05:29 +0800

    Add CI support for MetaX backend (#2528)

--- tools/setup_metax.sh
+++ tools/setup_metax.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pip install -e .[metax,test]
+
+export MACA_PATH=/opt/maca
+export LD_LIBRARY_PATH=$MACA_PATH/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$MACA_PATH/mxgpu_llvm/lib$LD_LIBRARY_PATH
+# For FlagTree
+export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib/python3.12/site-packages/triton/backends/metax/lib:$LD_LIBRARY_PATH
