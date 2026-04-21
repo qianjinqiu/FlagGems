dd966c682d3d85c53eab07b11637f48eecf85c32
Qiming Teng <tengqm@outlook.com>
2026-04-21 13:11:47 +0800

    Reformat pyproject.toml (#2571)

--- tools/setup_nvidia.sh
+++ tools/setup_nvidia.sh
@@ -5,5 +5,5 @@ uv pip install -e . .[nvidia,test]
 if [ -n "${USE_FLAGTREE}" ]; then
   uv pip uninstall triton
   uv pip install --index ${FLAGOS_PYPI} \
-    flagtree==0.5.0+3.5
+    "flagtree==0.5.0+3.5"
 fi
