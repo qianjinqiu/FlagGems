4f5b57d7371ed07e275af8d450d2a7558e082273
chxyi <92632985+chxyi@users.noreply.github.com>
2026-04-24 15:19:39 +0800

    [FlagGems Operator Development Competition] add leaky relu operator (#1830)
    * added leaky relu operator
    
    * fix code style issues
    
    ---------
    
    Co-authored-by: yichaoxiong <yichaoxiong@jzy.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -165,6 +165,7 @@ from flag_gems.ops.isneginf import isneginf, isneginf_out
 from flag_gems.ops.kron import kron
 from flag_gems.ops.layernorm import layer_norm, layer_norm_backward
 from flag_gems.ops.le import le, le_scalar
+from flag_gems.ops.leaky_relu import leaky_relu, leaky_relu_, leaky_relu_out
 from flag_gems.ops.lerp import lerp_scalar, lerp_scalar_, lerp_tensor, lerp_tensor_
 from flag_gems.ops.lift_fresh_copy import lift_fresh_copy, lift_fresh_copy_out
 from flag_gems.ops.linspace import linspace
@@ -534,6 +535,9 @@ __all__ = [
     "kron",
     "layer_norm",
     "layer_norm_backward",
+    "leaky_relu",
+    "leaky_relu_",
+    "leaky_relu_out",
     "le",
     "le_scalar",
     "lerp_scalar",
