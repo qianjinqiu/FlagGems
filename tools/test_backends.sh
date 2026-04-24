6c91ec4025f20111fca48e926b4ba347f12ca5f0
Qiming Teng <tengqm@outlook.com>
2026-04-24 15:16:32 +0800

    Use flagtree by default for all platforms (#2637)

--- tools/test_backends.sh
+++ tools/test_backends.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
+
 # test_backends.sh <VENDOR> <PR_ID>
 
 echo $CHANGED_FILES
 
-SUPPORTED_VENDORS=(
-  "ascend"
-  "hygon"
-  "iluvatar"
-  "kunlunxin"
-  "metax"
-  "mthreads"
-  "nvidia"
-  "thead"
-  "tsingmicro"
-)
-
-valid_vendor() {
-  needle=$1
-  for item in "${SUPPORTED_VENDORS[@]}" ; do
-    [ $item == "$needle" ] && return 0
-  done
-  return 1
-}
-
 [ "$#" -eq 2 ] || { echo "Please specify <VENDOR> and <PR_ID>"; exit 1; }
-VENDOR=${1}
-valid_vendor $VENDOR
-[ "$?" == 0 ] || { echo "Invalid vendor ${VENDOR} specified" ; exit 1; }
+
 PR_ID=${2}
 
 echo "Running FlagGems tests with VENDOR=${VENDOR}"
-
-# 1. Set virtual environment
-#    This is specific to the CI runners
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
-
-uv venv
-source .venv/bin/activate
 
 # 2. Install
 export FLAGOS_PYPI="https://resource.flagos.net/repository/flagos-pypi-${VENDOR}/simple"
